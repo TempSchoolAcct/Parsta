55cc7b7b2672e4f8eb9c54a5094614782fc17287
Test <fake@email.com>
2020-02-26 13:11:45 -0600

    Initial Commit

---
+++ Parsta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parsta
+//
+//  Created by Socstudent on 2/26/20.
+//  Copyright © 2020 Socstudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
